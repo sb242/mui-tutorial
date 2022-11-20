73c934bdf55603979f937dc63f03ce30ab7b9dd9
Spencer <spencerb242@proton.me>
2022-11-20 00:46:57 -0700

    created modal for making posts

--- src/App.js
+++ src/App.js
@@ -3,17 +3,32 @@ import Feed from "./components/Feed";
 import Rightbar from "./components/Rightbar";
 import Navbar from "./components/Navbar";
 import { Box, Container, Stack } from "@mui/system";
+import Add from "./components/Add";
+import { createTheme } from "@mui/material";
+import { useState } from "react";
+import { ThemeProvider } from "@emotion/react";
 
 function App() {
+  const [mode, setMode] = useState("light");
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
+
   return (
-    <Box>
-      <Navbar />
-      <Stack direction="row" spacing={2} justifyContent="space-between">
-        <Sidebar />
-        <Feed />
-        <Rightbar />
-      </Stack>
-    </Box>
+    <ThemeProvider theme={darkTheme}>
+      <Box>
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Sidebar />
+          <Feed />
+          <Rightbar />
+        </Stack>
+        <Add />
+      </Box>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Add.jsx
@@ -0,0 +1,104 @@
+import {
+  Delete,
+  EmojiEmotions,
+  PersonAdd,
+  VideoCameraBack,
+  Image,
+  DateRange,
+} from "@mui/icons-material";
+import {
+  Fab,
+  IconButton,
+  Modal,
+  Tooltip,
+  Typography,
+  styled,
+  Avatar,
+  TextField,
+  ButtonGroup,
+  Button,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import { Box, Stack } from "@mui/system";
+import { useState } from "react";
+
+const StyledModal = styled(Modal)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+const UserBox = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  marginBottom: "20px",
+});
+
+const Add = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <Tooltip
+        onClick={(e) => setOpen(true)}
+        title="Add Post"
+        sx={{
+          position: "fixed",
+          bottom: 20,
+          left: { xs: "calc(50% - 25px)", md: 30 },
+        }}
+      >
+        <Fab color="primary" aria-label="add">
+          <AddIcon />
+        </Fab>
+      </Tooltip>
+      <StyledModal
+        open={open}
+        onClose={(e) => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box width={400} height={280} bgcolor="white" p={3} borderRadius={5}>
+          <Typography variant="h6" color="gray" textAlign="center">
+            Create Post
+          </Typography>
+          <UserBox>
+            <Avatar
+              sx={{ width: 30, height: 30 }}
+              src="https://images.pexels.com/photos/762020/pexels-photo-762020.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            />
+            <Typography fontWeight={500} variant="span">
+              Sarah Jane
+            </Typography>
+          </UserBox>
+          <TextField
+            sx={{ width: "100%" }}
+            id="standard-multiline-static"
+            multiline
+            rows={2}
+            placeholder="What's happening cap'n?"
+            variant="standard"
+          />
+          <Stack direction="row" gap={1} mt={2} mb={2}>
+            <EmojiEmotions color="primary" />
+            <Image color="secondary" />
+            <VideoCameraBack color="success" />
+            <PersonAdd color="error" />
+          </Stack>
+          <ButtonGroup
+            fullWidth
+            variant="contained"
+            aria-label="outlined primary button group"
+          >
+            <Button>Post</Button>
+            <Button sx={{ width: "100px" }}>
+              <DateRange />
+            </Button>
+          </ButtonGroup>
+        </Box>
+      </StyledModal>
+    </>
+  );
+};
+
+export default Add;
