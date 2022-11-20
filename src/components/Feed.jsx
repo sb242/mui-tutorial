318a7d26bba7a95af62bd98e559a2d3ee8b39078
Spencer <spencerb242@proton.me>
2022-11-19 21:52:28 -0700

    added layout components

---
+++ src/components/Feed.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box } from "@mui/system";
+
+const Feed = () => {
+  return (
+    <Box bgcolor="pink" flex={4} p={2}>
+      Feed
+    </Box>
+  );
+};
+
+export default Feed;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
